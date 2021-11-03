ba69d8c3e605fb846fd2d70b7957adba86716138
avelycure <van.emelin2012@yandex.ru>
2021-11-03 22:38:57 +0300

    Added apiService di

--- app/src/main/java/com/avelycure/cryptostats/data/repo/CryptoRepo.kt
+++ app/src/main/java/com/avelycure/cryptostats/data/repo/CryptoRepo.kt
@@ -3,20 +3,14 @@ package com.avelycure.cryptostats.data.repo
 import com.avelycure.cryptostats.common.Constants
 import com.avelycure.cryptostats.data.api_service.GeminiApiService
 import io.reactivex.rxjava3.core.Observable
+import org.koin.core.component.KoinComponent
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class CryptoRepo(
-): ICryptoRepo {
-
-    val retrofit = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-        .baseUrl(Constants.BASE_URL)
-        .build()
-
-    val apiService = retrofit.create(GeminiApiService::class.java)
+    val apiService: GeminiApiService
+): ICryptoRepo, KoinComponent {
 
     override fun getCandles(symbol: String): Observable<List<List<Float>>> {
         return apiService.getCandles(symbol)
--- app/src/main/java/com/avelycure/cryptostats/di/AppModule.kt
+++ app/src/main/java/com/avelycure/cryptostats/di/AppModule.kt
@@ -1,13 +1,27 @@
 package com.avelycure.cryptostats.di
 
+import com.avelycure.cryptostats.common.Constants
+import com.avelycure.cryptostats.data.api_service.GeminiApiService
 import com.avelycure.cryptostats.data.repo.CryptoRepo
 import com.avelycure.cryptostats.data.repo.ICryptoRepo
 import com.avelycure.cryptostats.presentation.CryptoInfoViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 val appModule = module {
-    single<ICryptoRepo> {CryptoRepo()}
+    single<ICryptoRepo> { CryptoRepo(get()) }
 
     viewModel { CryptoInfoViewModel(get()) }
+
+    single<GeminiApiService> {
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .baseUrl(Constants.BASE_URL)
+            .build()
+            .create(GeminiApiService::class.java)
+    }
 }
