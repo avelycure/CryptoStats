a8c3d6f70e1d0626d64413721610ac885be252e8
avelycure <van.emelin2012@yandex.ru>
2021-11-07 12:41:14 +0300

    Refactored ui

--- app/src/main/java/com/avelycure/cryptostats/presentation/CryptoInfoFragment.kt
+++ app/src/main/java/com/avelycure/cryptostats/presentation/CryptoInfoFragment.kt
@@ -1,11 +1,15 @@
 package com.avelycure.cryptostats.presentation
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
@@ -26,7 +30,6 @@ import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.math.roundToInt
-
 
 class CryptoInfoFragment : Fragment() {
     private lateinit var lineChart: LineChart
@@ -84,7 +87,8 @@ class CryptoInfoFragment : Fragment() {
                                 it.close
                             )
                         }
-                )
+                ),
+                "Set 1"
             )
 
             updateStats(state.statistic)
@@ -144,104 +148,74 @@ class CryptoInfoFragment : Fragment() {
         currentTvAskPrice = view.findViewById(R.id.ci_tv_current_ask)
     }
 
-    private fun setCandleChart() {
-        candleChart.apply {
-            setDrawBorders(true)
-            axisLeft.apply {
-                setDrawGridLines(false)
-                setDrawLabels(false)
-            }
-            axisLeft.apply {
-                setDrawGridLines(false)
-                textColor = Color.WHITE
-            }
-            requestDisallowInterceptTouchEvent(true)
-
-
-            xAxis.setDrawGridLines(false)
-
-            xAxis.setDrawLabels(false)
-            xAxis.granularity = 1f
-            xAxis.isGranularityEnabled = true
-            xAxis.setAvoidFirstLastClipping(true)
-
-            legend.isEnabled = false
-        }
-    }
-
-    private fun plotCandleGraphic(data: ArrayList<CandleEntry>) {
-        if (data.isNotEmpty()) {
-            candleChart.apply {
-                description.isEnabled = false
-                legend.isEnabled = false
-                axisRight.isEnabled = false
-                xAxis.labelCount = 6
-
-                setBackgroundColor(Color.DKGRAY)
-                xAxis.setDrawGridLines(false)
-                setDrawGridBackground(false)
-
-                legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
-                axisLeft.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART)
-
-                xAxis.position = XAxis.XAxisPosition.BOTTOM
-                xAxis.setDrawLabels(true)
-                xAxis.setCenterAxisLabels(true)
-            }
-
-
-            val candleDataSet = CandleDataSet(data, "Set 1").apply {
-                color = Color.rgb(80, 80, 80)
-                shadowColor = Color.LTGRAY
-                shadowWidth = 2F
-                decreasingColor = Color.RED
-                decreasingPaintStyle = Paint.Style.FILL
-                increasingPaintStyle = Paint.Style.FILL
-                increasingColor = Color.GREEN
-                neutralColor = Color.BLUE
-                setDrawValues(false)
-            }
-
-            candleChart.data = CandleData(candleDataSet)
-            candleChart.invalidate()
-        }
-    }
-
     private fun setLineChart() {
         lineChart.apply {
-            xAxis.setDrawGridLines(false)
-            axisRight.isEnabled = false
-
-            setBackgroundColor(Color.DKGRAY)
-            setPinchZoom(false)
-            setDrawGridBackground(false)
-            isDragEnabled = false
-            isScaleXEnabled = false
-            isScaleYEnabled = false
-
-            legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
-
+            axisLeft.isEnabled = false
             description.isEnabled = false
             legend.isEnabled = false
-            axisLeft.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART)
+            isDragEnabled = false
+            isScaleYEnabled = false
+            isScaleXEnabled = false
+            legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
 
-            xAxis.labelCount = 6
+            setDrawGridBackground(false)
+            setPinchZoom(false)
 
-            xAxis.valueFormatter = object : ValueFormatter() {
-                override fun getFormattedValue(value: Float): String {
-                    return "-${(24F - value).roundToInt()}h"
-                }
+            activity?.getColorFromAttr(R.attr.colorSurface)?.let { setBackgroundColor(it) }
+            activity?.getColorFromAttr(R.attr.colorOnSurface)?.let { setNoDataTextColor(it) }
+
+            axisRight.apply {
+                textColor = activity?.getColorFromAttr(R.attr.colorOnSurface) ?: Color.BLACK
+
+                setDrawGridLines(false)
+                setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART)
             }
 
-            xAxis.position = XAxis.XAxisPosition.BOTTOM
-            xAxis.setDrawLabels(true)
-            xAxis.setCenterAxisLabels(true)
+            xAxis.apply {
+                labelCount = 6
+                position = XAxis.XAxisPosition.BOTTOM
+
+                setDrawGridLines(false)
+                setDrawLabels(true)
+                setCenterAxisLabels(true)
+
+                valueFormatter = object : ValueFormatter() {
+                    override fun getFormattedValue(value: Float): String {
+                        return "-${(24F - value).roundToInt()}h"
+                    }
+                }
+            }
         }
     }
 
-    fun getFormatTimeWithTZ(currentTime: Date): String {
-        val timeZoneDate = SimpleDateFormat("h:mm a", Locale.getDefault())
-        return timeZoneDate.format(currentTime)
+    private fun setCandleChart() {
+        candleChart.apply {
+            description.isEnabled = false
+            legend.isEnabled = false
+
+            activity?.getColorFromAttr(R.attr.colorOnSurface)?.let { setNoDataTextColor(it) }
+            activity?.getColorFromAttr(R.attr.colorSurface)?.let { setBackgroundColor(it) }
+
+            setDrawGridBackground(false)
+            setDrawBorders(false)
+
+            xAxis.apply {
+                labelCount = 6
+                position = XAxis.XAxisPosition.BOTTOM
+                setDrawGridLines(false)
+                setDrawLabels(true)
+                setCenterAxisLabels(true)
+
+                textColor = activity?.getColorFromAttr(R.attr.colorOnSurface) ?: Color.BLACK
+            }
+
+            axisLeft.apply {
+                textColor = activity?.getColorFromAttr(R.attr.colorOnSurface) ?: Color.BLACK
+                setDrawGridLines(false)
+                setDrawLabels(false)
+                setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART)
+            }
+        }
     }
 
     private fun plotLineGraphic(data1: ArrayList<Entry>) {
@@ -254,6 +228,15 @@ class CryptoInfoFragment : Fragment() {
         lineChart.invalidate()
     }
 
+    private fun plotCandleGraphic(data: ArrayList<CandleEntry>, label: String) {
+        if (data.isNotEmpty()) {
+            val candleDataSet = makeCandleSet(data, label)
+
+            candleChart.data = CandleData(candleDataSet)
+            candleChart.invalidate()
+        }
+    }
+
     private fun makeLineSet(data: ArrayList<Entry>, label: String): LineDataSet {
         val set = LineDataSet(data, label)
 
@@ -264,11 +247,40 @@ class CryptoInfoFragment : Fragment() {
             setDrawFilled(true)
 
             fillDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.gradient)
-            color = Color.RED
+            color = activity?.getColorFromAttr(R.attr.colorOnSecondary) ?: Color.BLACK
             lineWidth = 2F
             mode = LineDataSet.Mode.CUBIC_BEZIER
             cubicIntensity = 0.2F
         }
         return set
+    }
+
+    private fun makeCandleSet(data: ArrayList<CandleEntry>, label: String): CandleDataSet {
+        return CandleDataSet(data, label).apply {
+            color = Color.rgb(80, 80, 80)
+            shadowColor = Color.LTGRAY
+            shadowWidth = 2F
+            decreasingColor = Color.RED
+            decreasingPaintStyle = Paint.Style.FILL
+            increasingPaintStyle = Paint.Style.FILL
+            increasingColor = Color.GREEN
+            neutralColor = Color.BLUE
+            setDrawValues(false)
+        }
+    }
+
+    @ColorInt
+    fun Context.getColorFromAttr(
+        @AttrRes attrColor: Int,
+        typedValue: TypedValue = TypedValue(),
+        resolveRefs: Boolean = true
+    ): Int {
+        theme.resolveAttribute(attrColor, typedValue, resolveRefs)
+        return typedValue.data
+    }
+
+    fun getFormatTimeWithTZ(currentTime: Date): String {
+        val timeZoneDate = SimpleDateFormat("h:mm a", Locale.getDefault())
+        return timeZoneDate.format(currentTime)
     }
 }
