c3be1a8d1863f2390fc43a6e52b6a887a82f3f01
avelycure <van.emelin2012@yandex.ru>
2021-11-08 20:32:04 +0300

    Added interface for checking network status

---
+++ app/src/main/java/com/avelycure/cryptostats/data/network/INetworkStatus.kt
@@ -0,0 +1,10 @@
+package com.avelycure.cryptostats.data.network
+
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+
+interface INetworkStatus {
+    fun isOnline(): Observable<Boolean>
+
+    fun isOnlineSingle(): Single<Boolean>
+}
