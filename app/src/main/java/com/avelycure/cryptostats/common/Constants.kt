b4db10d3ef38833c0909215933f79ee99bf9d013
avelycure <van.emelin2012@yandex.ru>
2021-11-15 15:38:40 +0300

    Added symbols for coins and for money

--- app/src/main/java/com/avelycure/cryptostats/common/Constants.kt
+++ app/src/main/java/com/avelycure/cryptostats/common/Constants.kt
@@ -2,4 +2,15 @@ package com.avelycure.cryptostats.common
 
 object Constants {
     const val BASE_URL = "https://api.gemini.com/"
+
+    val coinSymbol = mapOf(
+        "btc" to "Bitcoin",
+        "eth" to "Ethereum",
+        "doge" to "Dogecoin",
+    )
+
+    val moneySymbol = mapOf(
+        "usd" to "$",
+        "eur" to "€",
+    )
 }
